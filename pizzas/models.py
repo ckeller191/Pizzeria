1aae066cbcd8b94412b6a2ee36d90df635ea5942
Chris Keller <ckeller191@gmail.com>
2021-04-26 13:49:55 -0500

    before class 4/26

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+
+        def __str__(self):
+            return self.name
+
+
+class Toppings(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
