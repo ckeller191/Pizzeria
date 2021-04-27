22d328332877fd85a457da6ef5f5da4df5e309ca
Chris Keller <ckeller191@gmail.com>
2021-04-26 19:13:50 -0500

    installed pizzas in settings file

--- pizzas/models.py
+++ pizzas/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
 
 
 class Toppings(models.Model):
