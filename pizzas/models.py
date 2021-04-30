26d68c898db0ec34407aab1bbd48a8dc6ff1deb2
Chris Keller <ckeller191@gmail.com>
2021-04-30 17:57:36 -0500

    should be all done

--- pizzas/models.py
+++ pizzas/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/', null=True)
 
     def __str__(self):
         return self.name
